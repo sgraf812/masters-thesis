3bffb9b591a7c5d62835cb77b8a803414f313f77
Sebastian Graf <sgraf1337@gmail.com>
2017-07-24 12:01:57 +0200

    Just specialise on infer, so that I don't have to justify stuff

--- thesis/benchmarks/fixup.pl
+++ thesis/benchmarks/fixup.pl
@@ -6,7 +6,7 @@ use warnings;
 my $cols = scalar(@ARGV)-4;
 my $min = $ARGV[$cols+1];
 my $max = $ARGV[$cols+2];
-my $allregressions = $ARGV[$cols+3];
+my $includeinfer = $ARGV[$cols+3];
 
 my $skipped = 0;
 while (<STDIN>) {
@@ -18,7 +18,7 @@ while (<STDIN>) {
 	# any interesting ratio? e.g. not in [$min,$max]
 	# also we include all regressions (they have a plus) if that's what is wanted
 	my @ratios = /(-?\d{1,2}.\d)\\%/g;
-	if ((grep { $_ < $min || $max < $_ } @ratios) || ($allregressions && /\+/g)) {
+	if ((grep { $_ < $min || $max < $_ } @ratios) || ($includeinfer && /infer/g)) {
 		s/^([^ ]*?) /\\progname{$1} /;
 		print;
 	} else {
